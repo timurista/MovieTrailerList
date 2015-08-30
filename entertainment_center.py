4c500c7e1d5ff180a287945180a634d179ec5d63
unknown <tim.urista@ORV-FAC-50160.basisschools.local>
2015-08-30 11:18:04 -0700

    added movie files

---
+++ entertainment_center.py
@@ -0,0 +1,76 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie(title="Toy Story", 
+	storyline="A story of a boy and his toys that come to life.",
+	poster_image_url="http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+	trailer="https://www.youtube.com/watch?v=vwyZH85NQC4",
+	actors=[
+		("Tom Hanks","Woody (voice)"),
+		("Tim Allen","Buzz Lightyear (voice)"),
+		("Don Rickles","Mr. Potato Head (voice)"),
+		("Jim Varney","Slinky Dog (voice)"),
+		("Wallace Shawn","Rex (voice)"),
+		("John Ratzenberger","Hamm (voice)"),
+		("Annie Potts","Bo Peep (voice)"),
+		("John Morris","Andy (voice)"),
+		("Erik von Detten","Sid (voice)"),
+		("Laurie Metcalf","Mrs. Davis (voice)"),
+		("R. Lee Ermey","Sergeant (voice)"),
+		("Sarah Freeman","Hannah (voice)"),
+		("Penn Jillette","TV Announcer (voice)"),
+		("Jack Angel","Shark / Rocky Gibraltar (voice)"),
+	],
+	release_date="November 19, 1995",
+	)
+
+# print toy_story.storyline
+
+avatar = media.Movie(title="Avatar",
+	storyline="A marine on an alien planet.",
+	poster_image_url="http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+	trailer="https://www.youtube.com/watch?v=5PSNL1qE6VY",
+		actors=[("Sam Worthington","Jake Sully"),
+		("Zoe Saldana","Neytiri (as Zoe Saldana)"),
+		("Sigourney Weaver","Dr. Grace Augustine"),
+		("Stephen Lang","Colonel Miles Quaritch"),
+		("Michelle Rodriguez","Trudy Chacon"),
+		("Giovanni Ribisi","Parker Selfridge"),
+		("Joel David Moore","Norm Spellman"),
+		("CCH Pounder","Moat"),
+		("Wes Studi","Eytukan"),
+		("Dileep Rao","Dr. Max Patel"),
+		("Matt Gerald","Corporal Lyle Wainfleet"),
+		("Sean Anthony Moran","Private Fike"),
+		("Jason Whyte","Cryo Vault Med Tech"),
+		("Scott Lawrence","Venture Star Crew Chief"),
+	],
+	release_date="December 18, 2009",
+
+	)
+# print avatar.storyline
+
+starwars_iv = media.Movie(title="Star Wars IV: A New Hope",
+	storyline="A farmboy fights to save his way of life from the evil empire.",
+	poster_image_url="https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
+	trailer="https://www.youtube.com/watch?v=vZ734NWnAHA",
+	actors=[("Mark Hamill","Luke Skywalker"),
+		("Harrison Ford","Han Solo"),
+		("Carrie Fisher","Princess Leia Organa"),
+		("Peter Cushing","Grand Moff Tarkin"),
+		("Alec Guinness","Ben Obi-Wan Kenobi"),
+		("Anthony Daniels","C-3PO"),
+		("Kenny Baker","R2-D2"),
+		("Peter Mayhew","Chewbacca"),
+		("David Prowse","Darth Vader"),
+		],
+	release_date="May 25, 1977",
+	)
+
+movies = [starwars_iv,toy_story,avatar]
+print media.Movie.VALID_RATINGS
+print avatar.title
+print starwars_iv.get_cast()
+print avatar.get_cast()
+print toy_story.get_cast()
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,37 @@
+import webbrowser
+import urllib2
+
+class Video():
+	"""Video related class to inherit from"""
+	def __init__(self,**kwargs):
+		self.title = kwargs.get("title","")
+		self.duration = kwargs.get("duration","")
+		self.imdb_link = kwargs.get("imdb_link","")
+		self.actors = kwargs.get("actors",[])
+
+	def get_cast(self):
+		htmlList = '<ul id="displayCast">'
+		for (actor,role) in self.actors:
+			htmlList+='<li>'+actor + ' as <strong>' + role+'</strong></li>'
+		return htmlList+'</ul>'
+
+class Movie(Video):
+	"""This class provides way to store information for movies"""
+	VALID_RATINGS = ["G","PG","PG-13","R"]
+
+	def __init__(self, **kwargs):
+		Video.__init__(self,**kwargs)
+		self.storyline = kwargs.get("storyline","")
+		self.poster_image_url = kwargs.get("poster_image_url","")
+		self.trailer_youtube_url = kwargs.get("trailer","")
+		self.release_date=kwargs.get("release_date","")
+
+	# show movie trailer
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
+
+	def set_actors(self,actors):
+		assert len(actors)>0,"actors must be an array"
+		# must be in tuple
+		assert len(actors[1])>1,"actors must be in tuple"
+		self.actors = actors or []
